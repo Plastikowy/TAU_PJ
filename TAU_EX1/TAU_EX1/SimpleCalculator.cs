e849e2ca25e2a2e5b14e1d586d990428822321b7
DESKTOP-KQUR29E\Tomek <tplasticsamol@gmail.com>
2021-12-03 00:40:31 +0100

    cw1 completed

---
+++ TAU_EX1/TAU_EX1/SimpleCalculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAU_EX1
+{
+    public class SimpleCalculator
+    {
+        private float number1;
+        private float number2;
+        private float result;
+
+        public float Number1 { get => number1; set => number1 = value; }
+        public float Number2 { get => number2; set => number2 = value; }
+        public float Result { get => result; private set => result = value; }
+
+        public SimpleCalculator(float number1, float number2)
+        {
+            this.number1 = number1;
+            this.number2 = number2;
+        }
+
+        public SimpleCalculator()
+        {
+        }
+
+        public float Add()
+        {
+            result = number1 + number2;
+            return result;
+        }
+
+        public float Substract()
+        {
+            result = number1 - number2;
+            return result;
+        }
+
+        public float Divide()
+        {
+            if (number1 == 0 || number2 == 0)
+                throw new ArgumentException("You can't divide by 0");
+
+            result = number1 / number2;
+            return result;
+        }
+
+        public float Multiply()
+        {
+            result = number1 * number2;
+            return result;
+        }
+    }
+}
---
+++ TAU_EX1/TAU_EX1_Tests/SimpleCalculatorTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TAU_EX1;
+
+namespace TAU_EX1_Tests
+{
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        private SimpleCalculator calculator; 
+
+        [SetUp]
+        public void SetUp()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [Test]
+        public void CheckIfCalculatorIsNotNull()
+        {
+            Assert.IsNotNull(calculator);
+        }
+
+        [Test]
+        public void CheckCantDivideByZero()
+        {
+            calculator = new SimpleCalculator(2, 0);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                calculator.Divide();
+            });
+        }
+
+        [Test]
+        public void CanSetNumber1()
+        {
+            calculator.Number1 = 1;
+
+            Assert.AreEqual(calculator.Number1, 1);
+        }
+
+        [Test]
+        public void CanSetNumber2()
+        {
+            calculator.Number2 = 2;
+
+            Assert.AreEqual(calculator.Number2, 2);
+        }
+
+        [Test]
+        public void CheckAddResultCorrect()
+        {
+            calculator = new SimpleCalculator(2, 2);
+            float result = calculator.Add();
+
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void CheckSubstractResultCorrect()
+        {
+            calculator = new SimpleCalculator(2, 2);
+            float result = calculator.Substract();
+
+            Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void CheckDivideResultCorrect()
+        {
+            calculator = new SimpleCalculator(2, 2);
+            float result = calculator.Divide();
+
+            Assert.AreEqual(result, 1);
+        }
+
+        [Test]
+        public void CheckMultiplyResultCorrect()
+        {
+            calculator = new SimpleCalculator(2, 2);
+            float result = calculator.Multiply();
+
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void CheckCalculatorInstanceNotChange()
+        {
+            calculator.Number1 = 2;
+            calculator.Number2 = 4;
+
+            calculator.Add();
+
+            SimpleCalculator ein = calculator;
+
+            calculator.Number1 = 5;
+            calculator.Number2 = 5;
+
+            calculator.Multiply();
+
+            SimpleCalculator zwei = calculator;
+
+            Assert.AreEqual(ein, zwei);
+        }
+
+        [Test]
+        public void ResultsDiffersPerOperation()
+        {
+            calculator.Number1 = 2;
+            calculator.Number2 = 4;
+
+            float res1 = calculator.Add();
+
+            calculator.Number1 = 5;
+            calculator.Number2 = 5;
+
+            float res2 = calculator.Multiply();
+
+            Assert.AreNotEqual(res1, res2);
+        }
+    }
+}
